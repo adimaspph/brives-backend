4da931eabfa5018cfbdaab83a24636b4fefdb93c
adimaspph <adimas0101@gmail.com>
2022-03-16 22:45:40 +0700

    make model database

--- src/main/java/test/bta/brivesc09/repository/StaffDb.java
+++ src/main/java/test/bta/brivesc09/repository/StaffDb.java
@@ -1,11 +1,11 @@
-package test.bta.brivesc09.repository;
-
-import test.bta.brivesc09.model.StaffModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import java.util.Optional;
-
-@Repository
-public interface StaffDb extends JpaRepository<StaffModel, Long>{
-    Optional<StaffModel> findByIdStaff(Long idStaff);
-}
+//package test.bta.brivesc09.repository;
+//
+//import test.bta.brivesc09.model.StaffModel;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//import java.util.Optional;
+//
+//@Repository
+//public interface StaffDb extends JpaRepository<StaffModel, Long>{
+//    Optional<StaffModel> findByIdStaff(Long idStaff);
+//}
