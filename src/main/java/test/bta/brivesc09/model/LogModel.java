61ebdc4874c130c04af7b56440c2d8d31d4db194
Salman Marsha <salman.marsha@ui.ac.id>
2022-03-17 16:25:12 +0700

    merge

--- src/main/java/test/bta/brivesc09/model/LogModel.java
+++ src/main/java/test/bta/brivesc09/model/LogModel.java
@@ -4,26 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-<<<<<<< HEAD
-import org.springframework.format.annotation.DateTimeFormat;
-=======
->>>>>>> 4da931eabfa5018cfbdaab83a24636b4fefdb93c
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-<<<<<<< HEAD
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import java.io.Serializable;
-import java.time.LocalTime;
-=======
-import java.io.Serializable;
-import java.util.List;
->>>>>>> 4da931eabfa5018cfbdaab83a24636b4fefdb93c
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -33,31 +17,6 @@ import java.util.List;
 public class LogModel implements Serializable {
 
     @Id
-<<<<<<< HEAD
-    @Column(name = "id_log", nullable = false)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idLog;
-    
-    @NotNull
-    @Column(name = "tanggal",nullable = false)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    private LocalTime tanggal;
-
-    @NotNull
-    @Column(name = "deskripsi", nullable = false)
-    private String deskripsi;
-
-    @NotNull
-    @Column(name="status_kehadiran")
-    private Integer statusKehadiran;
-
-    @NotNull
-    @Column(name="bukti_kehadiran")
-    private String buktiKehadiran;
-
-    // relasi dengan pengajar
-
-=======
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idLog;
 
@@ -79,5 +38,4 @@ public class LogModel implements Serializable {
     private StaffModel staff;
 
     //Bukti Image
->>>>>>> 4da931eabfa5018cfbdaab83a24636b4fefdb93c
 }
