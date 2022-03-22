1abcf12afd7a3622c874caf6d32ee0567d08b5b5
Salman Marsha <salman.marsha@ui.ac.id>
2022-03-22 12:29:01 +0700

    PBI 5: Fix minor bugs

--- src/main/java/test/bta/brivesc09/service/UserRestServiceImpl.java
+++ src/main/java/test/bta/brivesc09/service/UserRestServiceImpl.java
@@ -78,7 +78,7 @@ public class UserRestServiceImpl implements UserRestService {
             message += "Password harus terdiri atas lebih dari 8 karakter;";
         }
         // 2) username udah terdaftar + nomor pegawai udah terdaftar
-        if (userDb.findByUsername(staff.getUsername()).get() != null) {
+        if (getUserByUsername(staff.getUsername()) != null) {
             message += "Username telah terdaftar;";
         }
         return message;
