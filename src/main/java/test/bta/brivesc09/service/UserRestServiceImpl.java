bb1ad60141432f0ceecf0ccaaf448eb0da120a18
Salman Marsha <salman.marsha@ui.ac.id>
2022-04-24 20:43:32 +0700

    adding password constraint

--- src/main/java/test/bta/brivesc09/service/UserRestServiceImpl.java
+++ src/main/java/test/bta/brivesc09/service/UserRestServiceImpl.java
@@ -95,12 +95,43 @@ public class UserRestServiceImpl implements UserRestService {
         // check condition
         // 1) password lebih dari 8 karakter
         if (staff.getPassword().length() < 8) {
-            message += "Password harus terdiri atas lebih dari 8 karakter;";
+            message += "Password harus terdiri atas lebih dari 8 karakter; ";
         }
         // 2) username udah terdaftar + nomor pegawai udah terdaftar
         if (getUserByUsername(staff.getUsername()) != null) {
-            message += "Username telah terdaftar;";
+            message += "Username telah terdaftar; ";
         }
+
+        char[] alphabet = staff.getPassword().toCharArray();
+        boolean hasCapital = false;
+        boolean hasDigit = false;
+        boolean hasLetter = false;
+
+        for (char c : alphabet) {
+            if (Character.isDigit(c)) {
+                hasDigit = true;
+            }
+            if (Character.isUpperCase(c)) {
+                hasCapital = true;
+            }
+            if (Character.isLetter(c)) {
+                hasLetter = true;
+            }
+            
+        }
+
+        if (!hasCapital) {
+            message += "Password harus terdiri atas setidaknya 1 kapital; ";
+        }
+
+        if (!hasDigit) {
+            message += "Password harus terdiri atas setidaknya 1 angka; ";
+        }
+
+        if (!hasLetter) {
+            message += "Password harus terdiri atas setidaknya 1 huruf; ";
+        }
+
         return message;
     }
 
