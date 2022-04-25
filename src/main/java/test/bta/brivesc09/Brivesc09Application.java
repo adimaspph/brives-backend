bcc49b03d22d74ef96136d78b6fd0a7ded929eb4
adimaspph <adimas0101@gmail.com>
2022-04-26 00:11:15 +0700

    deploy to digital ocean

--- src/main/java/test/bta/brivesc09/Brivesc09Application.java
+++ src/main/java/test/bta/brivesc09/Brivesc09Application.java
@@ -1,14 +1,22 @@
 package test.bta.brivesc09;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class Brivesc09Application {
+public class Brivesc09Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+//		new SpringApplicationBuilder(Brivesc09Application.class)
+//				.web(WebApplicationType.SERVLET)
+//				.run(args);
 		SpringApplication.run(Brivesc09Application.class, args);
 	}
 
@@ -16,6 +24,10 @@ public class Brivesc09Application {
   	public BCryptPasswordEncoder bCryptPasswordEncoder() {
     	return new BCryptPasswordEncoder();
 	}
-    
+
+//	@Bean
+//	public ServletWebServerFactory servletWebServerFactory() {
+//		return new TomcatServletWebServerFactory();
+//	}
 
 }
